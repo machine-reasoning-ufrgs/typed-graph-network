b02aade2573940929a5e784da951d309ef102a30
phcavelar <9154669+phcavelar@users.noreply.github.com>
2019-04-18 10:51:30 -0300

    Added randomization on problem loading

--- neurosat/instance_loader.py
+++ neurosat/instance_loader.py
@@ -4,7 +4,7 @@ from cnf import CNF, BatchCNF, create_batchCNF
 from functools import reduce
 
 
-class InstanceLoader(object):
+class InstanceLoaderSequential(object):
 
   def __init__(self,path):
     assert os.path.isdir( path ), "Path is not a directory. Path {}".format( path ) 
@@ -15,7 +15,7 @@ class InstanceLoader(object):
     sat_folder = path + '/sat/'
     unsat_folder = path + '/unsat/'
     
-    self.filenames = reduce(lambda x,y: x + y, [ [sat.path,unsat.path] for (sat,unsat) in zip(os.scandir(sat_folder),os.scandir(unsat_folder)) ])
+    self.filenames = reduce(lambda x,y: x + y, sorted([ (sat.path,unsat.path) for (sat,unsat) in zip(os.scandir(sat_folder),os.scandir(unsat_folder)) ]),[])
 
     self.reset()
   #end
@@ -39,48 +39,52 @@ class InstanceLoader(object):
 #end
 
 
-#class InstanceLoader(object):
-#
-#  def __init__(self,path):
-#    assert os.path.isdir( path ), "Path is not a directory. Path {}".format( path ) 
-#    if path[-1] == "/":
-#      path = path[0:-1]
-#    #end if
-#    folders = [path]
-#    self.filenames = []
-#    while len(folders)>0:
-#      newfolders = []
-#      for folder in folders:
-#        newfolders += [ f.path for f in os.scandir(folder) if f.is_dir() ]
-#        self.filenames += [ f.path for f in os.scandir(folder) if f.is_file() and f.path.endswith(".cnf") ]
-#      #end for
-#      folders = newfolders
-#    #end while
-#    self.reset()
-#  #end
-#
-#  def get_instances(self, n_instances):
-#    for i in range(n_instances):
-#      yield CNF.read_dimacs(self.filenames[self.index])
-#      self.index += 1
-#    #end
-#  #end
-#
-#  def get_batches(self, batch_size):
-#    for i in range( len(self.filenames) // batch_size ):
-#      yield create_batchCNF(self.get_instances(batch_size))
-#    #end
-#  #end
-#
-#  def reset(self):
-#    random.shuffle( self.filenames )
-#    self.index = 0
-#  #end
-##end
+class InstanceLoaderRandomPaired(object):
 
+  def __init__(self,path):
+    assert os.path.isdir( path ), "Path is not a directory. Path {}".format( path ) 
+    if path[-1] == "/":
+      path = path[0:-1]
+    #end if
+
+    sat_folder = path + '/sat/'
+    unsat_folder = path + '/unsat/'
+    
+    self.filenames = [ (sat.path,unsat.path) for (sat,unsat) in zip(os.scandir(sat_folder),os.scandir(unsat_folder)) ]
+    print( self.filenames )
+
+    self.reset()
+  #end
+
+  def get_instances(self, n_instances):
+    i = 0
+    while i < n_instances:
+      if i%2 == 0:
+        yield CNF.read_dimacs(self.filenames[self.index][0])
+      else:
+        yield CNF.read_dimacs(self.filenames[self.index][1])
+        self.index += 1
+      #end if-else
+      i += 1
+    #end
+  #end
+
+  def get_batches(self, batch_size):
+    for i in range( len(self.filenames) // batch_size ):
+      yield create_batchCNF(self.get_instances(batch_size))
+    #end
+  #end
+
+  def reset(self):
+    random.shuffle( self.filenames )
+    self.index = 0
+  #end
+#end InstanceLoaderRandom
+
+InstanceLoader = InstanceLoaderRandomPaired
 
 if __name__ == '__main__':
 
-  instance_loader = InstanceLoaderPaired("instances")
+  instance_loader = InstanceLoaderRandomPaired("test-instances")
 
 #end
